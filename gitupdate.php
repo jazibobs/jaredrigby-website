e9e0205453e88ec65aae0fd82dccb074a357876f
Jared Rigby <talk@jaredrigby.co.uk>
2013-05-27 22:03:04 +0100

    Updated bash script to point to master brance

--- gitupdate.php
+++ gitupdate.php
@@ -3,5 +3,5 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
  
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/ && git reset --hard HEAD && git pull origin master' );
 }
