18b8f34978e651746ac90e1b2d8797cfe628f906
Jared Rigby <talk@jaredrigby.co.uk>
2013-05-27 20:28:42 +0100

    Updated bash script again

--- gitupdate.php
+++ gitupdate.php
@@ -1,1 +1,7 @@
-<?php shell_exec ("git pull origin master 2>&1");
+<?php
+ 
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+ 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/ && git reset --hard HEAD && git pull' );
+}
