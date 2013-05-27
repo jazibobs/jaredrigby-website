d08a188a0d07282667b12ccc728e9f7342b49b1c
Jared Rigby <talk@jaredrigby.co.uk>
2013-05-28 00:32:22 +0100

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
 					<li><a class="linkedin" href="http://uk.linkedin.com/in/jaredgrigby/">LINKEDIN</a></li>
 					<li><a class="plus" href="https://plus.google.com/u/0/108846301542540516777/posts">GOOGLE+</a></li>
 				</ul>
-				<div id="copy">© 2012-2013 <a href="mailto:talk@jaredrigby.co.uk">JARED RIGBY</a></div>
+				<div id="copy">© 2012-2013 <a href="mailto:talk@jaredrigby.co.uk">BY JARED RIGBY</a></div>
 			</div>
 			
 		</div>
