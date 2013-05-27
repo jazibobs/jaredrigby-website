83e47f893d207c563e61512f9c47cd1151d5884f
Jared Rigby <talk@jaredrigby.co.uk>
2013-05-27 22:08:10 +0100

    Updated bash script to point to master branch

--- gitupdate.php
+++ gitupdate.php
@@ -1,7 +1,3 @@
 <?php
  
-// Use in the "Post-Receive URLs" section of your GitHub repo.
- 
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/ && git pull origin master' );
-}
+shell_exec( 'cd /var/www/ && git pull origin master' );
