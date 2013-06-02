b20da27f0ce03071430d116be28cce06abf01abe
Jared Rigby <talk@jaredrigby.co.uk>
2013-06-02 13:10:53 +0100

    Updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,11 @@
-			</div>
-			
+      </div>
+      			
 			<div id="footer">
 				<ul>
-					<li><a class="twitter" href="http://www.twitter.com/jaredrigby">TWITTER</a></li>
-					<li><a class="linkedin" href="http://uk.linkedin.com/in/jaredgrigby/">LINKEDIN</a></li>
-					<li><a class="plus" href="https://plus.google.com/u/0/108846301542540516777?rel=author">GOOGLE+</a></li>
+        	<li><a target="_blank" class="github" href="https://github.com/jazibobs">GITHUB</a></li>
+					<li><a target="_blank" class="twitter" href="http://www.twitter.com/jaredrigby">TWITTER</a></li>
+					<li><a target="_blank" class="linkedin" href="http://uk.linkedin.com/in/jaredgrigby/">LINKEDIN</a></li>
+					<li><a target="_blank" class="plus" href="https://plus.google.com/u/0/108846301542540516777">GOOGLE+</a></li>
 				</ul>
 				<div id="copy">© 2012-2013 <a href="mailto:talk@jaredrigby.co.uk">BY JARED RIGBY</a></div>
 			</div>
